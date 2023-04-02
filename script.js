9f305d9afdd7c953a148e75eceb6e78838431ede
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-04-02 01:34:31 -0500

    seperated functions into pexelsAPI

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ import { MAPBOX_API_KEY } from './keys.js';
 
 
 const PEXELS_API_KEY = PEXEL_API_KEY;
-
 const OPENWEATHERMAP_API_KEY = OPEN_WEATHER_APPID;
-
 const MAPBOX_ACCESS_TOKEN = MAPBOX_API_KEY;
 
 import { fetchCurrentWeather, fetchForecast, weatherKeywords } from './weatherAPI.js';
 
+import { appendImageToCard  } from './pexelsAPI.js';
 
 let map;
 
@@ -104,87 +103,87 @@ let map;
 // };
 
 
-function appendImageToCard(lat, lon) {
-	// Fetch current weather data first
-	fetchCurrentWeather(lat, lon)
-		.then(currentWeather => {
-			// Use current weather description and city name in the Pexels API query
-			let keyword = currentWeather.description.toLowerCase();
-			if (keyword in weatherKeywords) {
-				keyword = weatherKeywords[keyword];
-			} else {
-				keyword = currentWeather.description;
-			}
-			const pexelsUrl = `https://api.pexels.com/v1/search?query=${encodeURIComponent(keyword + ' ' + currentWeather.city)}&per_page=1&page=${Math.floor(Math.random() * 100)}`;
+// function appendImageToCard(lat, lon) {
+// 	// Fetch current weather data first
+// 	fetchCurrentWeather(lat, lon)
+// 		.then(currentWeather => {
+// 			// Use current weather description and city name in the Pexels API query
+// 			let keyword = currentWeather.description.toLowerCase();
+// 			if (keyword in weatherKeywords) {
+// 				keyword = weatherKeywords[keyword];
+// 			} else {
+// 				keyword = currentWeather.description;
+// 			}
+// 			const pexelsUrl = `https://api.pexels.com/v1/search?query=${encodeURIComponent(keyword + ' ' + currentWeather.city)}&per_page=1&page=${Math.floor(Math.random() * 100)}`;
+//
+// 			// Fetch forecast data next
+// 			fetchForecast(lat, lon)
+// 				.then(forecastData => {
+// 					// Create an array of Pexels API URLs to fetch images for each forecast day
+// 					const pexelsUrls = forecastData.map(day => {
+// 						let keyword = day.description.toLowerCase();
+// 						if (keyword in weatherKeywords) {
+// 							keyword = weatherKeywords[keyword];
+// 						} else {
+// 							keyword = day.description;
+// 						}
+// 						return `https://api.pexels.com/v1/search?query=${encodeURIComponent(keyword + ' ' + currentWeather.city)}&per_page=1&page=${Math.floor(Math.random() * 100)}`;
+// 					});
+//
+// 					// Add the current weather URL to the array of Pexels API URLs
+// 					pexelsUrls.unshift(pexelsUrl);
+//
+// 					// Use Promise.all() to fetch all the images at once
+// 					Promise.all(pexelsUrls.map(url => {
+// 						return fetch(url, {
+// 							headers: {
+// 								'Authorization': PEXELS_API_KEY
+// 							}
+// 						})
+// 							.then(response => response.json())
+// 					}))
+// 						.then(responses => {
+// 							// Extract the image URLs from each Pexels API response
+// 							const imageUrls = responses.map(response => {
+// 								return response.photos[0].src.large;
+// 							});
+//
+// 							// Create cards for both the current weather and the forecast data
+// 							const currentWeatherCard = createCard(imageUrls[0], currentWeather, 'Now');
+// 							const forecastCards = forecastData.map((day, index) => {
+// 								return createCard(imageUrls[index+1], day, day.date);
+// 							});
+//
+// 							// Combine the cards into a single string and add them to the card container
+// 							const cards = [currentWeatherCard, ...forecastCards].join('');
+// 							const cardContainer = document.getElementById('card-container');
+// 							cardContainer.innerHTML = cards;
+// 						})
+// 						.catch(error => {
+// 							console.error("Error fetching images from Pexels:", error);
+// 						});
+// 				})
+// 				.catch(error => {
+// 					console.error("Error fetching forecast data:", error);
+// 				});
+// 		})
+// 		.catch(error => {
+// 			console.error("Error fetching weather data:", error);
+// 		});
+// }
 
-			// Fetch forecast data next
-			fetchForecast(lat, lon)
-				.then(forecastData => {
-					// Create an array of Pexels API URLs to fetch images for each forecast day
-					const pexelsUrls = forecastData.map(day => {
-						let keyword = day.description.toLowerCase();
-						if (keyword in weatherKeywords) {
-							keyword = weatherKeywords[keyword];
-						} else {
-							keyword = day.description;
-						}
-						return `https://api.pexels.com/v1/search?query=${encodeURIComponent(keyword + ' ' + currentWeather.city)}&per_page=1&page=${Math.floor(Math.random() * 100)}`;
-					});
 
-					// Add the current weather URL to the array of Pexels API URLs
-					pexelsUrls.unshift(pexelsUrl);
-
-					// Use Promise.all() to fetch all the images at once
-					Promise.all(pexelsUrls.map(url => {
-						return fetch(url, {
-							headers: {
-								'Authorization': PEXELS_API_KEY
-							}
-						})
-							.then(response => response.json())
-					}))
-						.then(responses => {
-							// Extract the image URLs from each Pexels API response
-							const imageUrls = responses.map(response => {
-								return response.photos[0].src.large;
-							});
-
-							// Create cards for both the current weather and the forecast data
-							const currentWeatherCard = createCard(imageUrls[0], currentWeather, 'Now');
-							const forecastCards = forecastData.map((day, index) => {
-								return createCard(imageUrls[index+1], day, day.date);
-							});
-
-							// Combine the cards into a single string and add them to the card container
-							const cards = [currentWeatherCard, ...forecastCards].join('');
-							const cardContainer = document.getElementById('card-container');
-							cardContainer.innerHTML = cards;
-						})
-						.catch(error => {
-							console.error("Error fetching images from Pexels:", error);
-						});
-				})
-				.catch(error => {
-					console.error("Error fetching forecast data:", error);
-				});
-		})
-		.catch(error => {
-			console.error("Error fetching weather data:", error);
-		});
-}
-
-
-function createCard(imageUrl, weather, day) {
-	return `
-		<div class="card" style="width: 18rem;">
-			<img src="${imageUrl}" class="card-img-top" alt="Weather image">
-			<div class="card-body">
-				<h5 class="card-title">${day} - ${weather.temperature}°C</h5>
-				<p class="card-text">${weather.description}</p>
-			</div>
-		</div>
-	`;
-}
+// function createCard(imageUrl, weather, day) {
+// 	return `
+// 		<div class="card" style="width: 18rem;">
+// 			<img src="${imageUrl}" class="card-img-top" alt="Weather image">
+// 			<div class="card-body">
+// 				<h5 class="card-title">${day} - ${weather.temperature}°C</h5>
+// 				<p class="card-text">${weather.description}</p>
+// 			</div>
+// 		</div>
+// 	`;
+// }
 
 
 // appendImageToCard();

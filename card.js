aecbbafd62ff4b4746514ba2aec5ef097278b626
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-04-02 03:08:26 -0500

    styling the position of the cards

--- card.js
+++ card.js
@@ -1,11 +1,18 @@
+
 export function createCard(imageUrl, weather, day) {
 	return `
-		<div class="card" style="width: 18rem;">
-			<img src="${imageUrl}" class="card-img-top" alt="Weather image">
-			<div class="card-body">
-				<h5 class="card-title">${day} - ${weather.temperature}°C</h5>
-				<p class="card-text">${weather.description}</p>
-			</div>
-		</div>
-	`;
+    <div class="card mb-3 col-6 col-md-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${imageUrl}" class="card-img" alt="Weather image">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${day} - ${weather.temperature}°C</h5>
+            <p class="card-text">${weather.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
 }
